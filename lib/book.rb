5e502cdc0de4c766d652ad12014a796733ec2467
Brianna Jordan <bryvjordan9@gmail.com>
2020-01-09 19:08:26 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,6 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
 
   GENRES = []
 
@@ -15,6 +15,6 @@ class Book
   def genre=(genre)
     @genre = genre
     GENRES << genre
-  end 
+  end
 
 end
